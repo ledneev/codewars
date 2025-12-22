04460c6a8d40b4062577d061fc1a8769dec28faf
Ivan Lednev <feyrune33@yandex.ru>
2025-12-22 06:22:55 +0300

    Add solution - CodeHub

---
+++ 7-kyu/all-none-and-any/all-none-and-any.js
@@ -0,0 +1,26 @@
+Array.prototype.all = function (p) {
+  for (let i = 0; i < this.length; i++) {
+    if (!p(this[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+​
+Array.prototype.none = function (p) {
+  for (let i = 0; i < this.length; i++) {
+    if (p(this[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+​
+Array.prototype.any = function (p) {
+  for (let i = 0; i < this.length; i++) {
+    if (p(this[i])) {
+      return true;
+    }
+  }
+  return false;
+};
